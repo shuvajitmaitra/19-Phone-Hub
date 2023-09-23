877cd5ce5f7ff223664807dadfd124433e2316f6
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-09-23 20:25:29 +0600

    Initial Project setup

---
+++ src/Components/Header/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <div>
+      <h3>THis is banner</h3>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h3>THis is navbar</h3>
+    </div>
+  );
+};
+
+export default Navbar;
